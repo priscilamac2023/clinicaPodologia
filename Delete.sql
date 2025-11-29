03aff277ba24a7fd1806fa5a78c1013130807a8a
priscilamac2023 <priscilamac2023@gmail.com>
2025-11-29 14:58:10 -0300

    aplicar DELETE para limpeza de dados na tabela <Produto> <Tratamento>

---
+++ Delete.sql
@@ -0,0 +1,22 @@
+-----Delete Produto------
+
+Select * from Produto;
+
+Delete from Produto
+where id_produto = 6;
+
+select * from Produto;
+
+-----Delete Tratamento------
+
+Select * from Tratamento;
+
+DELETE from Tratamento
+WHERE id_tratamento = 11;
+
+Select * from Tratamento;
+
+DELETE from Tratamento
+WHERE id_tratamento = 12;
+
+select * from Tratamento;
