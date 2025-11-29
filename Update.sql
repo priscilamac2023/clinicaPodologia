addb75d4823628b1089dd992d462018c55acb3ec
priscilamac2023 <priscilamac2023@gmail.com>
2025-11-29 15:05:44 -0300

    atualizar registros na tabela <Tratamento><Produto> usando UPDATE

---
+++ Update.sql
@@ -0,0 +1,98 @@
+
+-----Tratamento------
+
+
+UPDATE Tratamento
+
+set nome = 'Spa dos pes'
+WHERE id_tratamento = 13;
+
+UPDATE Tratamento
+
+set descricao = 'massagem relaxante para os pés'
+WHERE id_tratamento = 13;
+
+UPDATE Tratamento
+
+set id_tratamento = 4
+WHERE id_tratamento = 13;
+
+UPDATE Tratamento
+
+set nome = 'Tratamento de Micose Plantar'
+WHERE id_tratamento = 14;
+
+UPDATE Tratamento
+
+set id_tratamento = 5
+WHERE id_tratamento = 14;
+
+UPDATE Tratamento
+
+set descricao = 'Cuidados específicos para micose plantar'
+WHERE id_tratamento = 14;
+
+Select * from Tratamento;
+
+UPDATE Tratamento
+
+set nome = 'Verruga Plantar'
+WHERE id_tratamento = 15;
+
+UPDATE Tratamento
+
+set id_tratamento = 6
+WHERE id_tratamento = 15;
+
+UPDATE Tratamento
+
+set descricao = 'Tratamento específico para verruga plantar(olho de peixe)'
+WHERE id_tratamento = 15;
+
+--UPDATE Produto----
+
+UPDATE Produto
+
+set nome = 'Antifungico'
+WHERE id_produto = 5;
+
+
+UPDATE Produto
+
+set descricao = 'Tratamento para micose de unha'
+WHERE id_produto = 5;
+
+UPDATE Produto
+
+set preco = 55
+WHERE id_produto = 5;
+
+UPDATE Produto
+
+set nome = 'Óleo Ozonizado'
+WHERE id_produto = 7;
+
+UPDATE Produto
+
+set descricao = 'Auxilia na cicatrização de feridas'
+WHERE id_produto = 7;
+
+UPDATE Produto
+
+set preco = 70
+WHERE id_produto = 7;
+
+UPDATE Produto
+
+set nome = 'Serum para unhas e cutículas'
+WHERE id_produto = 8;
+
+UPDATE Produto
+
+set preco = 65
+WHERE id_produto = 8;
+
+UPDATE Produto
+
+set descricao = 'Hidratação para unhas e cutículas'
+WHERE id_produto = 8;
